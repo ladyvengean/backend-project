7a2584cfb6fdcee8076e154a468f635a67fcde3e
Shruti <shrutiwantstochil@gmail.com>
2025-01-18 22:46:56 +0530

    final setup

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -2,7 +2,7 @@ import mongoose, {isValidObjectId} from "mongoose"
 import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/Apierror.js"
 import {ApiResponse} from "../utils/Apiresponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import {asyncHandler} from "../utils/ayncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
